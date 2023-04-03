7d7a1e7705f856892faa1bcd4ac9f08998151b4a
Alexey Defusa <alexreal1999@gmail.com>
2023-04-03 19:58:44 +0300

    easy

---
+++ rectangleByA/rectangleByA/rectangleByA/rectangleByA.cpp
@@ -0,0 +1,15 @@
+﻿// rectangleByA.cpp : Defines the entry point for the application.
+//
+
+#include "rectangleByA.h"
+
+
+int main()
+{	
+	int height = 5;
+	int width = 8;
+
+
+	rectangle(&width,&height);
+	return 0;
+}
---
+++ rectangleByA/rectangleByA/rectangleByA/rectangleByA.h
@@ -0,0 +1,19 @@
+﻿// rectangleByA.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+void rectangle(int* width, int* height) {
+
+	for (int i = 0; i < *height; i++) {
+		for (int j = 0; j < *width; j++) {
+			std::cout << "A";
+		}
+		std::cout << "\n";
+	}
+
+}
+
+// TODO: Reference additional headers your program requires here.
